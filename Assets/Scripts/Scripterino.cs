9edbec142d54cc6429dd62a10ad2dd7106f1dcd2
Oliver Köchli <oli.koechli@outlook.com>
2018-03-31 23:58:56 +0200

    Building Click N2

--- Assets/Scripts/Scripterino.cs
+++ Assets/Scripts/Scripterino.cs
@@ -1,6 +1,4 @@
-﻿using MP_TD.App.Services;
-using MP_TD.Shared.Entities;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +6,7 @@ public class Scripterino : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        UnitService unitService = new UnitService();
-
-        Unit dog = unitService.GetById(1);
-        Debug.Log(dog.Damage);
+        
     }
 	
 	// Update is called once per frame
---
+++ Basics/MP_TD.App/Depots/BuildingDepot.cs
@@ -0,0 +1,17 @@
+﻿using MP_TD.Base;
+using MP_TD.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MP_TD.App.Depots
+{
+    public class BuildingDepot : BaseDepot<Building, int>
+    {
+        internal List<Building> GetAll()
+        {
+            return Database.Fetch<Building>(GetBaseSelectSql());
+        }
+    }
+}
